7115c662a9280cc7e9aa3b16b5d79861d228918e
Amr El-Madah <amr.nasser.elmadah@gmail.com>
2018-09-06 17:11:35 +0200

    Change the server key to make you able to test just for one time with that key

--- app/src/main/java/com/amr/tidetaskjava/utils/Constants.java
+++ app/src/main/java/com/amr/tidetaskjava/utils/Constants.java
@@ -2,7 +2,7 @@ package com.amr.tidetaskjava.utils;
 
 public class Constants {
     //TODO:You have to change that key to a new one every time you make api request or if you have quota in google console account you'll just change that key with your key
-    public static final String SERVER_KEY = "AIzaSyABn4wEp1k_Ps4i5WQUHwnBFPvsAjLy2L4";
+    public static final String SERVER_KEY = "AIzaSyBp6sF-qcgiXKYHygeR4p0I42T4_mUaATo";
 
     public static final String PLACES_TYPE = "bar";
     public static final int RADIUS = 1000;
